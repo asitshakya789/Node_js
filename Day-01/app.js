5754d9c82ce9cc03f982723f60e5cfd51023ce9c
asitshakya678 <asitshakya789@gmail.com>
2025-02-14 09:53:14 +0530

    Add initial app.js file with basic functionality and imports

---
+++ Day-01/app.js
@@ -0,0 +1,11 @@
+require('./xyz.js');
+ const {x,calculateSum} = require('./sum.js')
+
+var name =  "Asit kumar";
+var a = 10 ; 
+var b = 20;
+calculateSum(a,b);
+
+
+console.log(name); // Asit kumar
+console.log(x);
