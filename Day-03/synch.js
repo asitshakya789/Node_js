9434c6b90010def7fb79b0ee4c96c19a705f95f2
asitshakya678 <asitshakya789@gmail.com>
2025-02-20 22:44:41 +0530

    Implement asynchronous data fetching and add synchronous example files

---
+++ Day-03/synch.js
@@ -0,0 +1,12 @@
+
+let fun = function basicdetails() {
+    console.log("Name: ASit Kumar");
+    console.log("Age: 21");
+}
+let fun1 = function address() {
+    console.log("Address: Bhubaneswar");
+    console.log("Pin: 751003");
+
+    
+}
+fun();
