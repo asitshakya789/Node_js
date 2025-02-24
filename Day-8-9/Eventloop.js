65e50518d12f8687c8bd95659c2f93fbaad5b350
asitshakya678 <asitshakya789@gmail.com>
2025-02-24 23:26:44 +0530

    Add Event Loop example demonstrating setImmediate, setTimeout, and file reading

---
+++ Day-8-9/Eventloop.js
@@ -0,0 +1,19 @@
+const fs  = require('fs');
+const a =1000;
+
+setImmediate(()=>{
+    console.log('setImmediate');
+});
+fs.readFile("file.txt",'utf-8' , ()=>{
+    console.log('File Read');   
+});
+
+setTimeout(()=>{
+    console.log('Timeexpires');
+},0);
+
+function printa(){
+    console.log("A = ", a);
+}
+printa();
+console.log("End of the code");
